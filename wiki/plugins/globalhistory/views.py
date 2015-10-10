c9bbdbf9165b65379f379213ff01960e3a352818
Maximilien Cuony <maximilien@theglu.org>
2015-10-10 17:16:17 +0200

    Allow only comite to check history

--- wiki/plugins/globalhistory/views.py
+++ wiki/plugins/globalhistory/views.py
@@ -20,4 +20,7 @@ class GlobalHistory(ListView):
             request, *args, **kwargs)
 
     def get_queryset(self):
-        return self.model.objects.can_read(self.request.user).order_by('-modified')
+        if request.user.is_staff:
+            return self.model.objects.can_read(self.request.user).order_by('-modified')
+        else:
+            return None
